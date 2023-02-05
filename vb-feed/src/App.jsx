82df186ca509a34b0a86b62ef3e252b5c80b24d8
Edsonllima <focusti@hotmail.com>
2023-02-05 20:17:07 -0300

    add component Header

--- vb-feed/src/App.jsx
+++ vb-feed/src/App.jsx
@@ -1,15 +1,17 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Edson Lima"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
+        content="Lorem Ipsum is simply been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
       />
       <Post
         author="Heloise Lima"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
+        content="Lorem Ipsum is simply dummyunknown printer took a galley of type "
       />
     </div>
   );
