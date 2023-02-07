b3298b5db30e83421fca388e04095fecfa1b515d
Edsonllima <focusti@hotmail.com>
2023-02-07 00:37:17 -0300

    add component sidebar

--- vb-social/src/App.jsx
+++ vb-social/src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
+import style from "./App.module.css";
 
 import "./global.css";
 
@@ -7,14 +10,19 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Edson Lima"
-        content="Lorem Ipsum is simply been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
-      />
-      <Post
-        author="Heloise Lima"
-        content="Lorem Ipsum is simply dummyunknown printer took a galley of type "
-      />
+      <div className={style.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Edson Lima"
+            content="Lorem Ipsum is simply been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type "
+          />
+          <Post
+            author="Heloise Lima"
+            content="Lorem Ipsum is simply dummyunknown printer took a galley of type "
+          />
+        </main>
+      </div>
     </div>
   );
 }
