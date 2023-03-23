1905842df4983df2cdb3d16372251766550145b2
Edsonllima <focusti@hotmail.com>
2023-03-23 19:18:48 -0300

    create interface PostType

--- vb-social-ts/src/components/Post.tsx
+++ vb-social-ts/src/components/Post.tsx
@@ -17,27 +17,32 @@ interface Content {
   content: string;
 }
 
-interface PostProps {
+export interface PostType {
+  id: number;
   author: Author;
   publishedAt: Date;
   content: Content[];
 }
 
-export function Post({ author, publishedAt, content }: PostProps) {
+interface PostProps {
+  post: PostType;
+}
+
+export function Post({ post }: PostProps) {
   //estado = variáveis que eu quero que o componente monitore
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
   const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
-    publishedAt,
+    post.publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
     {
       locale: ptBR,
     }
   );
 
-  const publishedAtDateRalativeToNow = formatDistanceToNow(publishedAt, {
+  const publishedAtDateRalativeToNow = formatDistanceToNow(post.publishedAt, {
     locale: ptBR,
     addSuffix: true,
   });
@@ -78,22 +83,22 @@ export function Post({ author, publishedAt, content }: PostProps) {
     <article className={style.post}>
       <header>
         <div className={style.author}>
-          <Avatar src={author.avatarUrl} />
+          <Avatar src={post.author.avatarUrl} />
           <div className={style.authorInfo}>
-            <strong>{author.name}</strong>
-            <span>{author.role}</span>
+            <strong>{post.author.name}</strong>
+            <span>{post.author.role}</span>
           </div>
         </div>
         <time
           title={publishedDateFormatted}
-          dateTime={publishedAt.toISOString()}
+          dateTime={post.publishedAt.toISOString()}
         >
           {publishedAtDateRalativeToNow}
         </time>
       </header>
 
       <div className={style.content}>
-        {content.map((line) => {
+        {post.content.map((line) => {
           if (line.type === "paragraph") {
             return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
