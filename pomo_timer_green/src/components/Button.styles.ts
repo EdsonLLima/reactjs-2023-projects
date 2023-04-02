1208e705d888e8c67967ea31859ae6d9ecda6eed
Edsonllima <focusti@hotmail.com>
2023-04-02 00:09:31 -0300

    start project

---
+++ pomo_timer_green/src/components/Button.styles.ts
@@ -0,0 +1,18 @@
+.button {
+  width: 100px;
+  height: 40px;
+  color: aliceblue;
+}
+
+.primary {
+  background-color: purple;
+}
+.secondary {
+  background-color: orange;
+}
+.danger {
+  background-color: red;
+}
+.success {
+  background-color: green;
+}
