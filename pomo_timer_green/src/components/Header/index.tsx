08a3619efd49a72192e9e5ca24a56d6496754ea7
Edsonllima <focusti@hotmail.com>
2023-05-12 15:36:26 -0300

    change tag a for tag NavLink and change href for to

--- pomo_timer_green/src/components/Header/index.tsx
+++ pomo_timer_green/src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import { HeaderContainer } from './styles'
 import logoPomoTimer from '../../assets/logo-pomo_timer_green.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logoPomoTimer} alt="Dois tringulos em cor verde" />
       <nav>
-        <a href="#">timer</a>
-        <a href="#">history</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
