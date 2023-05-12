f0181d7c7d4e50e963a021f2c6506ff115ef5e2e
Edsonllima <focusti@hotmail.com>
2023-05-12 19:20:06 -0300

    import styles

--- pomo_timer_green/src/pages/Home/index.tsx
+++ pomo_timer_green/src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer } from './styles'
 
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form action="">
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <input type="text" id="task" />
 
@@ -12,21 +13,21 @@ export function Home() {
           <input type="number" id="minutesAmount" />
 
           <span>minutos.</span>
-        </div>
+        </FormContainer>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
           <span>:</span>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
         <button type="submit">
           <Play size={24} />
           Começar
         </button>
       </form>
-    </div>
+    </HomeContainer>
   )
 }
