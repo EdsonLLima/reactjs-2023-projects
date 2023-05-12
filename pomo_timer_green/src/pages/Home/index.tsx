47f0a7b3cc4ab36678d26c5f5de560b106747100
Edsonllima <focusti@hotmail.com>
2023-05-12 19:09:21 -0300

    add form

--- pomo_timer_green/src/pages/Home/index.tsx
+++ pomo_timer_green/src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
+import { Play } from 'phosphor-react'
+
 export function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
     </div>
   )
 }
