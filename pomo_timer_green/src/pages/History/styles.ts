71a6a923279ad69f163653a3890765c6fdf68dbc
Edsonllima <focusti@hotmail.com>
2023-05-19 11:17:39 -0300

    change status for <Status>

--- pomo_timer_green/src/pages/History/styles.ts
+++ pomo_timer_green/src/pages/History/styles.ts
@@ -59,3 +59,27 @@ export const HistoryList = styled.div`
     }
   }
 `
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  red: 'red-500',
+  green: 'green-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS // otimizado o typeof é por que o typescript não lê objeto JavaScript
+  // statusColor: 'yellow' | 'red' | 'green' -- estava assim
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
